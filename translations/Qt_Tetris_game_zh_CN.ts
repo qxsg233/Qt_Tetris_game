0969b0a0d6001dd869e0c4c1ec0b84a2af48bc05
qxsg233 <108057398+qxsg233@users.noreply.github.com>
2024-04-07 09:12:36 +0800

    增加了中文

--- translations/Qt_Tetris_game_zh_CN.ts
+++ translations/Qt_Tetris_game_zh_CN.ts
@@ -4,8 +4,48 @@
 <context>
     <name>Widget</name>
     <message>
-        <source>Widget</source>
-        <translation type="unfinished"></translation>
+        <source>Tetris</source>
+        <translation type="unfinished">俄罗斯方块</translation>
+    </message>
+    <message>
+        <source>start</source>
+        <translation type="unfinished">开始</translation>
+    </message>
+    <message>
+        <source>point</source>
+        <translation type="unfinished">分数</translation>
+    </message>
+    <message>
+        <source>next</source>
+        <translation type="unfinished">下一个</translation>
+    </message>
+    <message>
+        <source>Revolve</source>
+        <translation type="unfinished">旋转</translation>
+    </message>
+    <message>
+        <source>Left</source>
+        <translation type="unfinished">左</translation>
+    </message>
+    <message>
+        <source>Right</source>
+        <translation type="unfinished">右</translation>
+    </message>
+    <message>
+        <source>Down</source>
+        <translation type="unfinished">下</translation>
+    </message>
+    <message>
+        <source>over</source>
+        <translation type="unfinished">结束</translation>
+    </message>
+    <message>
+        <source>Tip</source>
+        <translation type="unfinished">提示</translation>
+    </message>
+    <message>
+        <source>game over</source>
+        <translation type="unfinished">游戏结束</translation>
     </message>
 </context>
 </TS>
